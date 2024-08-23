4c7203cee82d9ee30d32e7e0ef7c8c4d5ec278a4
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-24 00:17:45 +0530

    LinkedList

---
+++ src/DATA_STRUCTURES/LINKEDLIST/Main.java
@@ -0,0 +1,13 @@
+package DATA_STRUCTURES.LINKEDLIST;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(51);
+        list.insert(1);
+        list.insert(10);
+
+        list.show();
+    }
+}
---
+++ src/DATA_STRUCTURES/LINKEDLIST/Node.java
@@ -0,0 +1,6 @@
+package DATA_STRUCTURES.LINKEDLIST;
+
+public class Node {
+    int data;
+    Node next;
+}
