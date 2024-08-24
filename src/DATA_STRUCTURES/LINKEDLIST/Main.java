d7acc2d4cb29eaafd741b775e18c3c3e0c50cd09
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-24 23:50:48 +0530

    added insertAt and insertAtStart in LinkedList

--- src/DATA_STRUCTURES/LINKEDLIST/Main.java
+++ src/DATA_STRUCTURES/LINKEDLIST/Main.java
@@ -7,7 +7,9 @@ public class Main {
         list.insert(51);
         list.insert(1);
         list.insert(10);
-
+        list.insertAtStart(301);
+        list.insert(302);
+        list.insertAt(0, 102);
         list.show();
     }
 }
