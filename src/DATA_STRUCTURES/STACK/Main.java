52f1dc6c266bebfcb7b55dc918895673eb24727b
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-25 01:14:19 +0530

    DynamicStack with dynamic array using shrink and expand

--- src/DATA_STRUCTURES/STACK/Main.java
+++ src/DATA_STRUCTURES/STACK/Main.java
@@ -3,25 +3,34 @@ package DATA_STRUCTURES.STACK;
 
 public class Main {
     public static void main(String[] args) {
-        Stack nums = new Stack(5);
+        DynamicStack nums = new DynamicStack();
         System.out.println(nums.isEmpty());
         nums.push(15);
+        nums.show();
         nums.push(32);
+        nums.show();
         nums.push(6);
+        nums.show();
         nums.push(11);
+        nums.show();
         nums.push(19);
+        nums.show();
         nums.push(191);
-
-
         nums.show();
 
 
         nums.pop();
+        nums.show();
         nums.pop();
+        nums.show();
         nums.pop();
+        nums.show();
         nums.pop();
+        nums.show();
         nums.pop();
+        nums.show();
         nums.pop();
+        nums.show();
 
     }
 }
