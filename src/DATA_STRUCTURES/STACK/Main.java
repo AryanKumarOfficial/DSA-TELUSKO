674909e075b2b0b3cdc3d3d5e2beebf518b68d1c
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-25 00:51:48 +0530

    handled error in push and pop

--- src/DATA_STRUCTURES/STACK/Main.java
+++ src/DATA_STRUCTURES/STACK/Main.java
@@ -4,14 +4,24 @@ package DATA_STRUCTURES.STACK;
 public class Main {
     public static void main(String[] args) {
         Stack nums = new Stack(5);
+        System.out.println(nums.isEmpty());
         nums.push(15);
         nums.push(32);
         nums.push(6);
         nums.push(11);
         nums.push(19);
+        nums.push(191);
 
-        System.out.println(nums.pop());
-        System.out.println(nums.peek());
+
         nums.show();
+
+
+        nums.pop();
+        nums.pop();
+        nums.pop();
+        nums.pop();
+        nums.pop();
+        nums.pop();
+
     }
 }
